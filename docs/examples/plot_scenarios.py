d174d5ccd00b4ee32aacee5ceb77d04f9e6e7da5
Newton Sander <sandernewtonsander@gmail.com>
2023-07-20 08:23:12 +0200

    WIP

--- docs/examples/plot_scenarios.py
+++ docs/examples/plot_scenarios.py
@@ -13,7 +13,10 @@ import neurotechdevkit as ndk
 def plot_scenario(scenario_id):
     print(f"Simulating scenario: {scenario_id}")
     scenario = ndk.make(scenario_id)
+
+    # the line below breaks CI
     scenario.render_layout()
+
     result = scenario.simulate_steady_state()
     result.render_steady_state_amplitudes(show_material_outlines=False)
 
