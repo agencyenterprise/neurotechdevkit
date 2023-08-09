bf56bdcdfa8710f7e13f363e7fc3c5e560685978
charles <charles@ae.studio>
2023-08-09 11:05:52 -0700

    [#128] Fix Scenario-2 and Scenario-0 to work with different center frequencies (#132)
    * Resample scenario2 mask to the appropriate grid size
    
    * Add unit tests for initializing and compiling default scenarios
    
    * Add voxel to the dictionary
    
    * Remove the center_frequency=1e6 test because it is very slow for Scenario2_3D
    
    * Fix Scenario0 resizing bug
    
    * Port scenario 2 changes to new file

---
+++ tests/neurotechdevkit/scenarios/test_builtin.py
@@ -0,0 +1,39 @@
+"""Test built-in scenario 2."""
+
+import pytest
+
+from neurotechdevkit.scenarios.built_in import (
+    Scenario0,
+    Scenario1_2D,
+    Scenario1_3D,
+    Scenario2_2D,
+    Scenario2_3D,
+)
+
+
+@pytest.fixture(
+    params=[
+        Scenario0,
+        Scenario1_2D,
+        Scenario1_3D,
+        Scenario2_2D,
+        Scenario2_3D,
+    ]
+)
+def scenario_cls(request):
+    """Iterate over all scenarios"""
+    return request.param
+
+
+@pytest.fixture(params=[1e5, 5e5])
+def center_frequency(request):
+    """Iterate over different center frequencies"""
+    return request.param
+
+
+def test_compile_problem(scenario_cls, center_frequency):
+    """Test compiling each built-in scenario's default problem."""
+    scenario = scenario_cls()
+    scenario.center_frequency = center_frequency
+    scenario.make_grid()
+    scenario.compile_problem()
