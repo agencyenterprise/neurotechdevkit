b7c2dda811bf039906a133ddd66a44bf88f49639
Kyle McGraw <kyle@ae.studio>
2023-09-06 20:26:25 -0400

    [134] Slice `SteadyStateResult3D` to `SteadyStateResult2D` (#147)
    * Add method to slice 3d result
    
    * Add warning for metrics
    
    * Change target warning to use try/except
    
    * Change target getter, add checks for None target
    
    * Revert target getter, remove checks, add tests
    
    * Fix slicing alternate source types
    
    * Add suggestions, alter validate function
    
    * Drop duplicate _validate_slice_args

--- src/neurotechdevkit/scenarios/built_in/_scenario_1.py
+++ src/neurotechdevkit/scenarios/built_in/_scenario_1.py
@@ -8,7 +8,8 @@ import numpy.typing as npt
 from ... import rendering, sources
 from ...grid import Grid
 from ...materials import Material
-from .._base import Scenario, Scenario2D, Scenario3D, SliceAxis, Target
+from .._base import Scenario, Scenario2D, Scenario3D
+from .._utils import SliceAxis, Target
 
 
 class Scenario1(Scenario):
