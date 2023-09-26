5fd41a10f4a238730b585693a03e90089c16727c
charles <charles@ae.studio>
2023-09-26 14:02:10 -0700

    Add Benchmark 7 of Aubry et al. (2022) as a Scenario2 variant (#151)
    * Add Scenario2 variant that mirrors Benchmark 7 of Aubry et al. (2022)
    
    * Fix spellcheck

--- src/neurotechdevkit/scenarios/built_in/__init__.py
+++ src/neurotechdevkit/scenarios/built_in/__init__.py
@@ -1,7 +1,12 @@
 """Contain built-in scenarios for NDK."""
 from ._scenario_0 import Scenario0
 from ._scenario_1 import Scenario1_2D, Scenario1_3D
-from ._scenario_2 import Scenario2_2D, Scenario2_3D
+from ._scenario_2 import (
+    Scenario2_2D,
+    Scenario2_2D_Benchmark7,
+    Scenario2_3D,
+    Scenario2_3D_Benchmark7,
+)
 from ._scenario_3 import Scenario3
 
 __all__ = [
@@ -10,6 +15,8 @@ __all__ = [
     "Scenario1_3D",
     "Scenario2_2D",
     "Scenario2_3D",
+    "Scenario2_2D_Benchmark7",
+    "Scenario2_3D_Benchmark7",
     "Scenario3",
 ]
 
@@ -19,5 +26,7 @@ BUILT_IN_SCENARIOS = {
     "Scenario1_3D": Scenario1_3D,
     "Scenario2_2D": Scenario2_2D,
     "Scenario2_3D": Scenario2_3D,
+    "Scenario2_2D_Benchmark7": Scenario2_2D_Benchmark7,
+    "Scenario2_3D_Benchmark7": Scenario2_3D_Benchmark7,
     "Scenario3": Scenario3,
 }
