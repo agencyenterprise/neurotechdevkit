dea997f43991f671bef10dd7d8db7cdb2dc09e52
Newton Sander <sandernewtonsander@gmail.com>
2024-01-05 13:57:45 -0300

    First Vue.js commit

---
+++ src/web/app/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+const app = createApp(App);
+app.mount("#app");
