816ac600e44dfbb842e86e8de6ae02528d6ed0ce
Newton Sander <sandernewtonsander@gmail.com>
2024-07-26 08:29:54 +0200

    Replacing old app with new vue app

--- src/web/app.py
+++ src/web/app.py
@@ -1,14 +1,22 @@
 """Flask app factory."""
+
 import os
 import pathlib
+import shutil
+import zipfile
 
 import matplotlib
+import webbrowser
 from flask import Flask
 from flask_cors import CORS
 from matplotlib import rc
 from web.simulation_runner import SimulationRunner
 
 CT_FOLDER = pathlib.Path("./ct-scans/")
+WEB_SERVER_HOST = os.getenv("WEB_SERVER_HOST", "0.0.0.0")
+WEB_SERVER_PORT = os.getenv("WEB_SERVER_PORT", 8080)
+CURRENT_DIR = pathlib.Path(__file__).parent
+APP_DIR = CURRENT_DIR / "app"
 
 
 def create_app():
@@ -17,7 +25,7 @@ def create_app():
 
     rc("animation", html="html5")
 
-    app = Flask(__name__, static_folder="static", template_folder="templates")
+    app = Flask(__name__, static_folder="app/dist")
     CORS(app)
 
     # Register blueprints here
@@ -37,13 +45,26 @@ def assign_CT_FOLDER(app):
     app.config["CT_FOLDER"] = CT_FOLDER
 
 
+def extract_static_files():
+    """
+    Extract Vue build from dist.zip to app folder, so that it can be served by aiohttp
+    """
+    dist_dir = APP_DIR / "dist"
+    if os.path.exists(dist_dir) and os.path.isdir(dist_dir):
+        shutil.rmtree(dist_dir)
+    with zipfile.ZipFile(APP_DIR / "dist.zip", "r") as zip_ref:
+        zip_ref.extractall(APP_DIR / "dist")
+
+
 app = create_app()
+extract_static_files()
 
 
 def run():
     """Run the web app."""
     assign_CT_FOLDER(app)
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    webbrowser.open(f"http://{WEB_SERVER_HOST}:{WEB_SERVER_PORT}")
+    app.run(host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 
 if __name__ == "__main__":
